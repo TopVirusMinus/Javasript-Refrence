e68537e6ac755505d400080e76bf74fd1ae76003
TopVirusMinus <mwmma5000@gmail.com>
2020-10-10 16:38:29 +0200

    Added Classes

--- 11JavaScript Fundamentals.js
+++ 11JavaScript Fundamentals.js
@@ -251,3 +251,22 @@ console.log(person1.getYear())
 
 //this saves memory as i wont show it unless i need it
 
+//! Classes to make it prettier
+class Person{
+    constructor(firstName, lastName, dateOfBirth){
+        this.firstName= firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = new Date(dateOfBirth);
+    }
+    getYear() {
+        return this.dateOfBirth.getFullYear();
+    }
+    fullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+person1= new Person("Mustafa", "Walid", "9,16,2002")
+console.log(person1)
+console.log(person1.getYear())
+console.log(person1.fullName())
